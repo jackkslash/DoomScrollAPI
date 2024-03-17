9061f787deb35948671b9459b0df3e72b0c74d5c
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-03-17 18:36:59 +0000

    Refactored process route and store in db with existence check

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,9 +1,10 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
+import * as schema from './schema';
 
 const connectionString = process.env.DATABASE_URL
 // Disable prefetch as it is not supported for "Transaction" pool mode 
 const client = postgres(connectionString as string, { prepare: false })
-const db = drizzle(client);
+const db = drizzle(client, { schema });
 
 export default db;
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import { Actions, mediaObj } from './types'
 import db from './db/db'
 import { media } from './db/schema'
+import { eq } from 'drizzle-orm'
 
 require('dotenv').config()
 
@@ -24,37 +25,42 @@ function processLink(link: string) {
     // Define actions for each domain
     const actions: Actions = {
         'youtube.com': async () => {
-            const videoID = extractVideoId(link)
-            const res = await fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=" + videoID + "&key=" + process.env.YOUTUBE_APIKEY)
-            const body: any = await res.json();
-            const mediaObj: mediaObj = {
-                id: body.items[0].id,
-                title: body.items[0].snippet.title,
-                desc: body.items[0].snippet.description,
-                uploadDate: body.items[0].snippet.publishedAt,
-                channelId: body.items[0].snippet.channelId,
-                channelTitle: body.items[0].snippet.channelTitle,
-                thumbnails: {
-                    default: {
-                        url: body.items[0].snippet.thumbnails.default.url,
-                        width: body.items[0].snippet.thumbnails.default.width,
-                        height: body.items[0].snippet.thumbnails.default.height
-                    }
-                },
-                platform: 'youtube'
+            const videoID: any = extractVideoId(link)
+            const existingItem = await searchVideoID(videoID)
+            if (existingItem.length > 0) {
+                return existingItem
+            } else {
+                const res = await fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=" + videoID + "&key=" + process.env.YOUTUBE_APIKEY)
+                const body: any = await res.json();
+
+                const { items } = body
+                const [firstItem] = items
+                const { id, snippet } = firstItem
+                const { title, description, channelId, channelTitle, publishedAt, thumbnails } = snippet
+
+                const mediaObj: mediaObj = {
+                    id: id,
+                    title: title,
+                    desc: description,
+                    uploadDate: publishedAt,
+                    channelId: channelId,
+                    channelTitle: channelTitle,
+                    platform: 'youtube',
+                    thumbnails: thumbnails
+                }
+                await db.insert(media).values({
+                    id: mediaObj.id,
+                    title: mediaObj.title,
+                    desc: mediaObj.desc,
+                    uploadDate: mediaObj.uploadDate,
+                    channelId: mediaObj.channelId,
+                    channelTitle: mediaObj.channelTitle,
+                    thumbnails: mediaObj.thumbnails,
+                    platform: mediaObj.platform
+                })
+                return mediaObj;
             }
 
-            await db.insert(media).values({
-                id: mediaObj.channelId,
-                title: mediaObj.title,
-                desc: mediaObj.desc,
-                uploadDate: mediaObj.uploadDate,
-                channelId: mediaObj.channelId,
-                channelTitle: mediaObj.channelTitle,
-                thumbnails: mediaObj.thumbnails,
-                platform: mediaObj.platform
-            })
-            return mediaObj;
         },
         'tiktok.com': () => {
             console.log(link)
@@ -91,6 +97,15 @@ function extractVideoId(url: string) {
     const match = url.match(regExp);
     // Return video ID if found, otherwise return null
     return match ? match[1] : null;
+}
+
+async function searchVideoID(url: string) {
+    const q = await db.query.media.findMany({
+        where: eq(media.id, url)
+    })
+    console.log(q)
+    return q;
+
 }
 
 
