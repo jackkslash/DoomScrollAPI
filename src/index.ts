d4de3e3a47a5366170fc43e9dae97e877e32c4ed
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-03-18 18:33:33 +0000

    new media endpoint, query all with limit and offset

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,25 @@ app.get('/process', async (c) => {
     })
 })
 
+app.get('/media', async (c) => {
+    const limit = parseInt(c.req.query('limit') as string, 10) || 50;
+    const offset = parseInt(c.req.query('offset') as string, 10) || 0;
+
+    try {
+        const req = await db.query.media.findMany({
+            limit: limit,
+            offset: offset
+        });
+        console.log(req)
+        return c.json(req)
+        // Process req as needed
+    } catch (error) {
+        console.error('Error fetching media:', error);
+        c.status(500)
+        return c.body('Internal Server Error')
+    }
+})
+
 function processLink(link: string) {
     const domain = getDomain(link);
     // Define actions for each domain
