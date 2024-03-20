4ada3a47a7dc4f20aad2688ef412d27c21bf3fc6
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-03-20 22:34:38 +0000

    cors change

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import 'dotenv/config'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors';
 import { mediaRoute } from './routes/media'
 import { processRoute } from './routes/process'
 require('dotenv').config()
 
 const app = new Hono()
+app.use('*', cors({
+    origin: '*', // Be cautious, this allows all origins
+}));
+
 app.get('/', (c) => c.json({ message: "hello" }))
 app.route('/process', processRoute)
 app.route('/media', mediaRoute)
